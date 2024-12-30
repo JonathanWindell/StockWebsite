58a18f3b81972ccb07bd42436147e2cc2fe7b40b
Jonathan Windell <windelljonathan04@gmail.com>
2024-12-30 16:16:50 +0100

    commit testar charts

--- DatabaseFiles/Charts.py
+++ DatabaseFiles/Charts.py
@@ -1,3 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
+
+def fundchart():
+    labels = ["Information Technology", "Consumer Discretionary", "Financials", "Healthcare", "Communication Services",
+              "Industrials", "Consumer Staples", "Energy", "Utilities", "Materials"]
+    weight = [0.322, 0.117, 0.114, 0.099, 0.095, 0.820, 0.055, 0.310, 0.230, 0.018]
+
+    plt.figure(figsize=(10, 6))
+    plt.bar(labels, weight, color = "skyblue", edgecolor = "black")
+    plt.bar(labels, weight)
+    plt.xlabel("Sectors")
+    plt.ylabel("Weight")
+    plt.title("Fund Chart")
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    fundchart()
